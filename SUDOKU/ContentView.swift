4a11ad617db2c19e696cbb042419494ae5d3201a
sumeyraltas <sumeyraltas@gmail.com>
2023-11-18 13:33:34 +0300

    sudoku generated

--- SUDOKU/ContentView.swift
+++ SUDOKU/ContentView.swift
@@ -1,24 +1,82 @@
-//
-//  ContentView.swift
-//  SUDOKU
-//
-//  Created by Sumeyra Altas on 18.11.2023.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sudokuBoard: [[Int]] = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 9],
+        [6, 0, 0, 4, 2, 6, 1, 3, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            ForEach(0..<9) { row in
+                HStack {
+                    ForEach(0..<9) { column in
+                        SudokuCell(value: $sudokuBoard[row][column])
+                        if column == 2 || column == 5 {
+                            Divider().background(Color.black)
+                        }
+                    }
+                }
+                // Eğer satır 2 veya 5 ise yatay çizgi ekle
+                if row == 2 || row == 5 {
+                    Divider().background(Color.black)
+                }
+            }
+         
+            Button("Solve Sudoku") {
+                self.solveSudoku()
+            }
+            .padding()
+            .controlSize(.large)
+            .buttonStyle(.borderedProminent)
+            
         }
-        .padding()
+        
+    }
+
+    func solveSudoku() {
+        let solver = SudokuSolver(board: sudokuBoard)
+        if solver.solve() {
+            // If the solver finds a solution, update the UI with the solved values
+            sudokuBoard = solver.getBoard()
+        } else {
+            // Handle the case where no solution is found
+            print("No solution found.")
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct SudokuCell: View {
+    @Binding var value: Int
+
+    var body: some View {
+        TextField("", value: $value, formatter: NumberFormatter())
+            .textFieldStyle(SudokuTextFieldStyle())
+    }
+}
+
+struct SudokuTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<_Label>) -> some View {
+        configuration
+            .frame(width: 25, height: 25)
+            .border(Color.black, width: 0.3)
+            .multilineTextAlignment(.center)
+            .foregroundColor(.blue)
+            .font(.headline)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- SUDOKU/SudokuSolver.swift
+++ SUDOKU/SudokuSolver.swift
@@ -1,8 +1,75 @@
-//
-//  SudokuSolver.swift
-//  SUDOKU
-//
-//  Created by Sumeyra Altas on 18.11.2023.
-//
-
 import Foundation
+
+class SudokuSolver {
+    var board: [[Int]]
+
+    init(board: [[Int]]) {
+        self.board = board
+    }
+
+    private func isValid(_ row: Int, _ col: Int, _ num: Int) -> Bool {
+        // Check if the number is not in the current row and column
+        for i in 0..<9 {
+            if board[row][i] == num || board[i][col] == num {
+                return false
+            }
+        }
+
+        // Check if the number is not in the current 3x3 box
+        let boxStartRow = row - row % 3
+        let boxStartCol = col - col % 3
+        for i in 0..<3 {
+            for j in 0..<3 {
+                if board[boxStartRow + i][boxStartCol + j] == num {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    private func findEmptyLocation() -> (row: Int, col: Int)? {
+        for i in 0..<9 {
+            for j in 0..<9 {
+                if board[i][j] == 0 {
+                    return (i, j)
+                }
+            }
+        }
+        return nil
+    }
+
+    func solve() -> Bool {
+        guard let emptyLocation = findEmptyLocation() else {
+            // No empty location, puzzle is solved
+            return true
+        }
+
+        let row = emptyLocation.row
+        let col = emptyLocation.col
+
+        for num in 1...9 {
+            if isValid(row, col, num) {
+                board[row][col] = num
+
+                if solve() {
+                    return true
+                }
+
+                // If placing the number at (row, col) doesn't lead to a solution, backtrack
+                board[row][col] = 0
+            }
+        }
+
+        // No valid number found, need to backtrack
+        return false
+    }
+
+    func getBoard() -> [[Int]] {
+        return board
+    }
+}
+
+
+
