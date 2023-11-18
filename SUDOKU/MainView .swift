9b2d4d986e6ea6125fd4782f87d0052b34c91f50
sumeyraltas <sumeyraltas@gmail.com>
2023-11-18 19:17:49 +0300

    added levels buttons

---
+++ SUDOKU/MainView .swift
@@ -0,0 +1,18 @@
+//
+//  MainView .swift
+//  SUDOKU
+//
+//  Created by Sumeyra Altas on 18.11.2023.
+//
+
+import SwiftUI
+
+struct MainView_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView_()
+}
