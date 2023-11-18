6a04f306475473487712a5be076425394a4e64c5
sumeyraltas <sumeyraltas@gmail.com>
2023-11-18 12:09:35 +0300

    Initial Commit

---
+++ SUDOKU/SUDOKUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUDOKUApp.swift
+//  SUDOKU
+//
+//  Created by Sumeyra Altas on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUDOKUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
